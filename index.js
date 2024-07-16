b2257949da177031dd66866564619696693f59fb
Vlaqikk <ilinskyvladislav2002@mail.ru>
2024-07-16 20:28:48 +0500

    w

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end('WebSocket server is running');
+});
+
+const wss = new WebSocket.Server({ server });
+
+let remainingTime = 60; // Таймер на 60 секунд
+
+const timer = setInterval(() => {
+    remainingTime++;
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(remainingTime.toString());
+        }
+    });
+}, 1000);
+
+wss.on('connection', (ws) => {
+    ws.send(`Timer started: ${remainingTime} seconds`);
+
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+        // Обработка полученного сообщения
+        // Например, можно отправить сообщение обратно всем клиентам
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Received: ${message}`);
+            }
+        });
+    });
+});
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+server.listen(PORT, HOST, () => {
+    console.log('WebSocket server is running on ws://localhost:3000');
+});
