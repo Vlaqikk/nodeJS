c12f23536d7ac04fee7321195da42cf8758c7a39
Vlaqikk <ilinskyvladislav2002@mail.ru>
2024-07-16 22:40:36 +0500

    www

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const wss = new WebSocket.Server({ port: 5000 });
 let remainingTime = 60; // Таймер на 60 секунд
 
 const timer = setInterval(() => {
-    remainingTime--;
+    remainingTime++;
     console.log(remainingTime);
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
