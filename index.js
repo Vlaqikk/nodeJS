977b49b12f1a9cedf5e7a1d667cbda02544b19a5
Vlaqikk <ilinskyvladislav2002@mail.ru>
2024-07-16 23:23:13 +0500

    www

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const http = require('http');
 const ws = require('ws');
 
-const PORT = 8080;
-const wss = new ws.Server({ noServer: true });
+const wss = new ws.Server({noServer: true});
 console.log('-----');
-
 function accept(req, res) {
-  // Check for WebSocket upgrade
-  if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() !== 'websocket') {
+  // все входящие запросы должны использовать websockets
+  if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
     res.end();
     return;
   }
 
-  // Check for Connection header
+  // может быть заголовок Connection: keep-alive, Upgrade
   if (!req.headers.connection.match(/\bupgrade\b/i)) {
     res.end();
     return;
@@ -23,14 +21,15 @@ function accept(req, res) {
 
 function onConnect(ws) {
   ws.on('message', function (message) {
-    const name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu)?.[0] || "Гость";
+    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Гость";
     ws.send(`Привет с сервера, ${name}!`);
 
     setTimeout(() => ws.close(1000, "Пока!"), 5000);
   });
 }
 
-// Handle WebSocket connection
-wss.on('connection', (ws) => {
-  console.log('New client connected');
-});
+if (!module.parent) {
+  http.createServer(accept).listen(8080);
+} else {
+  exports.accept = accept;
+}
